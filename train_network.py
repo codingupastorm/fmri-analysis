06717390406049807019e1fcc41110b7a0ca4f52
Jordan Andrews <jordandrews@hotmail.com>
2024-05-14 19:59:31 +1000

    Closer to working

--- train_network.py
+++ train_network.py
@@ -14,47 +14,38 @@ social_data_path = r'ds000244-download\sub-01\ses-00\func\sub-01_ses-00_task-Arc
 spatial_img = image.load_img(spatial_data_path)
 social_img = image.load_img(social_data_path)
 
-full_data = image.concat_imgs([spatial_img, social_img])
+print("Transforming Data...")
 
-mask_img = masking.compute_epi_mask(full_data)
-spatial_data_masked = masking.apply_mask(spatial_img, mask_img)
-social_data_masked = masking.apply_mask(social_img, mask_img)
+# Flatten the spatial and social data
+spatial_data_flat = spatial_img.get_fdata().reshape(spatial_img.shape[3], -1)
+social_data_flat = social_img.get_fdata().reshape(social_img.shape[3], -1)
+
+print("Concating Data...")
+
+# Concatenate the flattened spatial and social data for masking
+full_data_flat = np.vstack((spatial_data_flat, social_data_flat))
+mask_img = masking.compute_epi_mask(spatial_img)
+mask_flat = mask_img.get_fdata().flatten()
+
+# Apply the mask to the flattened data
+X = full_data_flat[:, mask_flat.astype(bool)]
 
 # Create labels for each volume (0 for spatial, 1 for social)
-spatial_labels = np.zeros(spatial_data_masked.shape[0])
-social_labels = np.ones(social_data_masked.shape[0])
-
-# Combine the masked data and labels
-X = np.vstack((spatial_data_masked, social_data_masked))
-y = np.concatenate((spatial_labels, social_labels))
-
-print("Splitting Data...")
+y = np.concatenate((np.zeros(spatial_img.shape[3]), np.ones(social_img.shape[3])))
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print("Reshaping Data...")
-
-# Reshape the data to add a single channel dimension
-X_train = X_train.reshape((-1, *mask_img.shape))
-X_test = X_test.reshape((-1, *mask_img.shape))
-
-# Define the CNN model architecture
+# Define the model architecture
 model = Sequential()
-model.add(Conv3D(32, (3, 3, 3), activation='relu', input_shape=X_train.shape[1:]))
-model.add(MaxPooling3D((2, 2, 2)))
-model.add(Conv3D(64, (3, 3, 3), activation='relu'))
-model.add(MaxPooling3D((2, 2, 2)))
-model.add(Conv3D(128, (3, 3, 3), activation='relu'))
-model.add(MaxPooling3D((2, 2, 2)))
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 print("Compiling Model...")
 
 # Compile the model
-model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
 print("Training Model...")
 
